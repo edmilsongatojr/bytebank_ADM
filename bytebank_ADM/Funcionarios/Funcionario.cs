a19519cde0777efa7118c2ca6b010a038cf29c95
Jimmy <edygatojr@live.com>
2022-06-14 19:32:47 -0300

    Crianado a classe Funcionario e suas variaveis. Criado também a função getBonificacao

---
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario {
+
+        public string nome;
+        public string cpf;
+        public double salario;
+
+        public string Nome { get; set; }
+        public string Cpf { get; set;}
+        public double Salario { get; set; }
+
+
+        public double getBonificacao() {
+            return Salario * 0.1;
+        }
+
+    }
+}
--- bytebank_ADM/Program.cs
+++ bytebank_ADM/Program.cs
@@ -1,1 +1,1 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Boas Vindas ao ByteBank Administração");
